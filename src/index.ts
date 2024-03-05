25a0dc92fa0019091ce2d6cddac60c6d1c7fff19
Laura PAdrón <laupadron14528@gmail.com>
2024-03-05 18:59:04 -0300

    prueba

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-// import express from 'npm:express';
-// import tasksRoutes from './routes/tasks.routes.ts';
-// import './db.ts';
+import express from 'npm:express';
+import tasksRoutes from './routes/tasks.routes.ts';
+import './db.ts';
 
-// const app = express();
+const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
-// app.use(tasksRoutes)
+app.use(tasksRoutes)
 
-// app.listen(3000, () =>{
-//     console.log('server is running')
-// })
-import { Application,Router } from "@oak/oak/application";
+app.listen(3000, () =>{
+    console.log('server is running')
+})
+// import { Application,Router } from "@oak/oak/application";
 
-const app = new Application();
+// const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello CFW!";
-});
+// app.use((ctx) => {
+//   ctx.response.body = "Hello CFW!";
+// });
 
-const posts = new Router()
-  .get("/", (ctx) => {
-    ctx.response.body = `Forum: ${ctx.params.forumId}`;
-  })
-  .get("/:postId", (ctx) => {
-    ctx.response.body =
-      `Forum: ${ctx.params.forumId}, Post: ${ctx.params.postId}`;
-  });
+// const posts = new Router()
+//   .get("/", (ctx) => {
+//     ctx.response.body = `Forum: ${ctx.params.forumId}`;
+//   })
+//   .get("/:postId", (ctx) => {
+//     ctx.response.body =
+//       `Forum: ${ctx.params.forumId}, Post: ${ctx.params.postId}`;
+//   });
 
-export default { fetch: app.fetch };
+// export default { fetch: app.fetch };
